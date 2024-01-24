580d2a42e780ea5c0766ad9b69af5bfa3709a50b
OMOLLO PINCHEZ <masimevanistelrooy@gmail.com>
2024-01-24 13:06:38 +0300

    NEW

--- src/Components/Public.js
+++ src/Components/Public.js
@@ -4,9 +4,14 @@ import { Link } from "react-router-dom";
 import React from 'react'
 
 const Public = () => {
-  return (
-    <div>Public</div>
-  )
+ const content = (
+<section className="public">
+<header>
+<h1> Welcome To <span>INTO TECH REPAIRS</span></h1>
+</header>
+</section>
+
+ )
 }
 
 export default Public
