b24a3d8b2f7ea3fbc08559751b2bcba2f8933603
OMOLLO PINCHEZ <masimevanistelrooy@gmail.com>
2024-01-28 13:59:20 +0300

    nwe

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import { Route, Routes} from 'react-router-dom';
-import './App.css';
-import Layout from './Components/Layout';
-import Public from './Components/Public';
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Public from './components/Public'
 import Login from './features/auth/Login';
-import DashLayout from './Components/DashLayout';
+import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
-import NotesLists from './features/notes/NotesList'
-import UsersLists from './features/users/UsersList'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
 
 function App() {
- return (
- 
+  return (
     <Routes>
-      <Route path="*" element={<Layout />}> {/* options Add trailing * here */}
+      <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
 
-        <Route path="dash" element={<DashLayout />}> {/* options Add trailing * here */}
+        <Route path="dash" element={<DashLayout />}>
+
           <Route index element={<Welcome />} />
+
           <Route path="notes">
-            <Route index elemenet={<NotesLists />} />
+            <Route index element={<NotesList />} />
           </Route>
 
           <Route path="users">
-            <Route index elemenet={<UsersLists />} />
+            <Route index element={<UsersList />} />
           </Route>
 
         </Route>{/* End Dash */}
+
       </Route>
     </Routes>
-   
- );
+  );
 }
 
 export default App;
