00f0d69048091e0068ded0b0cb2ed11ee0072a17
OMOLLO PINCHEZ <masimevanistelrooy@gmail.com>
2024-01-31 13:15:34 +0300

    new

--- src/Components/Public.js
+++ src/Components/Public.js
@@ -3,22 +3,34 @@ import { Link } from 'react-router-dom'
 const Public = () => {
     const content = (
         <section className="public">
-            <header>
-                <h1>Welcome to <span className="nowrap">IntoTech Repairs!</span></h1>
+            <header className='public__header'>
+                <h1 className="nowrap "> IntoTech Repairs</h1>
             </header>
             <main className="public__main">
-                <p>Located in Beautiful Downtown  City, IntoTech Repairs. Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+            <h3>Welcome to <span className="nowrap1 ">IntoTech Repairs!</span></h3>
+                <p>We are located in Beautiful Downtown  City, IntoTech Repairs. <br/> Repairs  provides a trained staff ready to meet your tech repair needs. Lets bring your machine back to life again</p>
+                {/*<address className="public__addr">
+                IntoTech Repairs<br />
+                    555 Night Drive<br />
+                    Downtown City,  13345<br />
+                    <a href="tel:+254714924394">(+254) 7149-24394</a>
+                </address>
+    <br />*/}
+               
+            </main>
+            <footer>
+                <Link to="/login">Employee Login</Link>
+                <p className='FindUs'>Find Us At</p>
+                <div className='addr'>
                 <address className="public__addr">
                 IntoTech Repairs<br />
                     555 Night Drive<br />
                     Downtown City,  13345<br />
                     <a href="tel:+254714924394">(+254) 7149-24394</a>
                 </address>
+                </div>
                 <br />
                
-            </main>
-            <footer>
-                <Link to="/login">Employee Login</Link>
             </footer>
         </section>
 
